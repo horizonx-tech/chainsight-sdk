fd0cf352a71da1d424af7858dac4ad19314a2ac1
hide-yoshi <hide-yoshi@users.noreply.github.com>
2023-10-23 15:47:52 +0900

    remove unused

--- chainsight-cdk/src/web3/processors.rs
+++ chainsight-cdk/src/web3/processors.rs
@@ -72,8 +72,6 @@ impl EVMTransactionOptionBuilder {
                 call_options,
             )
             .await?;
-        // expected json: {"eip1559": true} or {"eip1559": false}
-        ic_cdk::println!("eip1559_support: {:?}", execution);
         Ok(self.convert_result(execution))
     }
 
