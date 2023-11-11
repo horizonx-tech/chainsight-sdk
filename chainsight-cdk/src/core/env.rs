be946fac8b9f1adff3a59827f26f2bab90926f4f
darroze <darroze@horizonx.tech>
2023-11-11 17:50:29 +0900

    update initializer

--- chainsight-cdk/src/core/env.rs
+++ chainsight-cdk/src/core/env.rs
@@ -12,7 +12,7 @@ impl Env {
     pub fn initializer(&self) -> Principal {
         match self {
             Env::LocalDevelopment => Principal::from_text("7fpuj-hqaaa-aaaal-acg7q-cai").unwrap(),
-            Env::Production => Principal::from_text("vvqfh-4aaaa-aaaao-a2mua-cai").unwrap(),
+            Env::Production => Principal::from_text("yhoh2-qaaaa-aaaao-a2n4a-cai").unwrap(),
             Env::Test => Principal::from_text("qoctq-giaaa-aaaaa-aaaea-cai").unwrap(), //TODO
         }
     }
