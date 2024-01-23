c989efc1dac9b5d6dabb30e24913d99e279c65fa
megared05 <05megared@gmail.com>
2024-01-23 17:30:26 +0900

    update: examples

--- chainsight-cdk-macros/examples/minimum_indexers/src/relayer/src/lib.rs
+++ chainsight-cdk-macros/examples/minimum_indexers/src/relayer/src/lib.rs
@@ -10,6 +10,7 @@ def_relayer_canister!(
         \"method_identifier\":\"get_last_snapshot : () -> (record { value : text; timestamp : nat64; })\",
         \"extracted_field\": \"value\",
         \"destination_type_to_convert\": \"U256\",
+        \"exponent_of_power10\": 18,
         \"abi_file_path\":\"__interfaces/Uint256Oracle.json\",
         \"lens_targets\":null,
         \"method_name\":\"update_state\"
