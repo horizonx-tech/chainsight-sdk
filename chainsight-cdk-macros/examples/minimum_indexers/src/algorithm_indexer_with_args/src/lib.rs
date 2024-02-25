72cbbe696009d35056cb7d376e3117f326d01e38
megared05 <05megared@gmail.com>
2024-02-25 20:56:39 +0900

    update: example - algorithm_indexer

--- chainsight-cdk-macros/examples/minimum_indexers/src/algorithm_indexer_with_args/src/lib.rs
+++ chainsight-cdk-macros/examples/minimum_indexers/src/algorithm_indexer_with_args/src/lib.rs
@@ -1,7 +1,8 @@
 use candid::{CandidType, Decode, Encode, Principal};
 use chainsight_cdk::{indexer::IndexingConfig, storage::Data};
 use chainsight_cdk_macros::{
-    algorithm_indexer_source, algorithm_indexer_with_args, chainsight_common, did_export, init_in,
+    algorithm_indexer_source, algorithm_indexer_with_args, chainsight_common, did_export,
+    generate_queries_for_key_value_store_struct, generate_queries_for_key_values_store_struct, init_in,
     manage_single_state, setup_func, timer_task_func, KeyValueStore, KeyValuesStore, Persist,
     StableMemoryStorable,
 };
@@ -36,17 +37,20 @@ pub struct TransferEvent {
 pub struct TotalSupply {
     value: String,
 }
+generate_queries_for_key_value_store_struct!(TotalSupply);
 
 #[derive(Debug, Clone, CandidType, Default, Deserialize, Serialize, Persist, KeyValueStore)]
 #[memory_id(2)]
 pub struct Balance {
     value: String,
 }
+generate_queries_for_key_value_store_struct!(Balance);
 
 #[derive(Debug, Clone, CandidType, Default, Deserialize, Serialize, Persist, KeyValuesStore)]
 #[memory_id(1)]
 pub struct Account {
     value: String,
 }
+generate_queries_for_key_values_store_struct!(Account);
 
 did_export!("example_canister");
